ebea07ca14b3319b4e4df0f2627d23fb67ff0613
George Sudarkoff <george@sudarkoff.com>
2014-08-25 22:41:24 -0700

    Change icon, add Hue as a notification.

--- BetterLaundryMonitor.groovy
+++ BetterLaundryMonitor.groovy
@@ -22,8 +22,8 @@ definition(
   author: "George Sudarkoff",
   description: "Using a switch with powerMonitor capability, monitor the laundry cycle and alert when it's done.",
   category: "Green Living",
-  iconUrl: "https://s3.amazonaws.com/smartapp-icons/GreenLiving/Cat-GreenLiving.png",
-  iconX2Url: "https://s3.amazonaws.com/smartapp-icons/GreenLiving/Cat-GreenLiving@2x.png")
+  iconUrl: "https://s3.amazonaws.com/smartthings-device-icons/Appliances/appliances8-icn.png",
+  iconX2Url: "https://s3.amazonaws.com/smartthings-device-icons/Appliances/appliances8-icn@2x.png")
 
 
 preferences {
@@ -39,8 +39,12 @@ preferences {
     input "sendPushMessage", "bool", title: "Send a push notification?"
   }
 
-  section (title: "More Options", hidden: hideOptionsSection(), hideable: true) {
-    input "phone", "phone", title: "Additionally, also send a text message to:", required: false
+  section (title: "Additionally", hidden: hideOptionsSection(), hideable: true) {
+    input "phone", "phone", title: "Send a text message to:", required: false
+    input "switches", "capability.switch", title: "Turn on this switch", required:false, multiple:true
+    input "hues", "capability.colorControl", title: "Turn these hue bulbs", required:false, multiple:true
+    input "color", "enum", title: "This color", required: false, multiple:false, options: ["White", "Red","Green","Blue","Yellow","Orange","Purple","Pink"]
+    input "lightLevel", "enum", title: "This light Level", required: false, options: [[10:"10%"],[20:"20%"],[30:"30%"],[40:"40%"],[50:"50%"],[60:"60%"],[70:"70%"],[80:"80%"],[90:"90%"],[100:"100%"]]
   }
 }
 
@@ -73,10 +77,79 @@ def handler(evt) {
   // If the washer stops drawing power, the cycle is complete, send notification.
   else if (state.cycleOn && latestPower == 0) {
     send(message)
+    lightAlert(evt)
     state.cycleOn = false
     state.cycleEnd = now()
     duration = state.cycleEnd - state.cycleStart
     log.trace "Cycle ended after ${duration} minutes."
+  }
+}
+
+private lightAlert(evt) {
+  def hueColor = 0
+  def saturation = 100
+
+  if (hues) {
+      switch(color) {
+          case "White":
+              hueColor = 52
+              saturation = 19
+              break;
+          case "Daylight":
+              hueColor = 53
+              saturation = 91
+              break;
+          case "Soft White":
+              hueColor = 23
+              saturation = 56
+              break;
+          case "Warm White":
+              hueColor = 20
+              saturation = 80 //83
+              break;
+          case "Blue":
+              hueColor = 70
+              break;
+          case "Green":
+              hueColor = 39
+              break;
+          case "Yellow":
+              hueColor = 25
+              break;
+          case "Orange":
+              hueColor = 10
+              break;
+          case "Purple":
+              hueColor = 75
+              break;
+          case "Pink":
+              hueColor = 83
+              break;
+          case "Red":
+              hueColor = 100
+              break;
+      }
+
+      state.previous = [:]
+
+      hues.each {
+          state.previous[it.id] = [
+              "switch": it.currentValue("switch"),
+              "level" : it.currentValue("level"),
+              "hue": it.currentValue("hue"),
+              "saturation": it.currentValue("saturation")
+          ]
+      }
+
+      log.debug "current values = $state.previous"
+
+      def newValue = [hue: hueColor, saturation: saturation, level: lightLevel as Integer ?: 100]
+      log.debug "new value = $newValue"
+
+      if (switches) {
+          switches*.on()
+      }
+      hues*.setColor(newValue)
   }
 }
 
@@ -93,6 +166,6 @@ private send(msg) {
 }
 
 private hideOptionsSection() {
-    (phone) ? false : true
+    (phone || switches || hues || color || lightLevel) ? false : true
 }
 
