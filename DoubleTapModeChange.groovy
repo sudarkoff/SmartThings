480e737e19037dff8fcf4610f00e0993bae04263
George Sudarkoff <georges@surveymonkey.com>
2014-12-20 19:41:07 -0800

    Limit SmartApp by time. Add an option to flash the lights as a confirmation.

--- DoubleTapModeChange.groovy
+++ DoubleTapModeChange.groovy
@@ -1,5 +1,5 @@
 /**
- *  Double Tap Mode Switch
+ *  Double Tap Switch for "Hello, Home" Action
  *
  *  Copyright 2014 George Sudarkoff
  *
@@ -15,17 +15,24 @@
  */
 
 definition(
-    name: "Double Tap Mode Switch",
-    namespace: "sudarkoff.com",
+    name: "Double Tap Switch for 'Hello, Home' Action",
+    namespace: "com.sudarkoff",
     author: "George Sudarkoff",
-    description: "Execute a 'Hello, Home' phrase when an existing switch is tapped twice in a row.",
+    description: "Execute a 'Hello, Home' action when an existing switch is tapped twice in a row.",
     category: "Mode Magic",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/ModeMagic/Cat-ModeMagic.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/ModeMagic/Cat-ModeMagic@2x.png"
+    iconUrl: "https://s3.amazonaws.com/smartthings-device-icons/Appliances/appliances17-icn.png",
+    iconX2Url: "https://s3.amazonaws.com/smartthings-device-icons/Appliances/appliances17-icn@2x.png"
 )
 
 preferences {
     page (name: "configApp")
+
+    page (name: "timeIntervalInput", title: "Only during a certain time") {
+        section {
+            input "starting", "time", title: "Starting", required: false
+            input "ending", "time", title: "Ending", required: false
+        }
+    }
 }
 
 def configApp() {
@@ -43,31 +50,51 @@ def configApp() {
             }
         }
 
+        section (title: "Notification method") {
+            input "sendPushMessage", "bool", title: "Send a push notification?"
+        }
+
         section (title: "More Options", hidden: hideOptionsSection(), hideable: true) {
+            input "phone", "phone", title: "Additionally, also send a text message to:", required: false
+            input "flashLights", "capability.switch", title: "And flash these lights", multiple: true, required: false
+
+            def timeLabel = timeIntervalLabel()
+            href "timeIntervalInput", title: "Only during a certain time", description: timeLabel ?: "Tap to set", state: timeLabel ? "complete" : null
+
             input "days", "enum", title: "Only on certain days of the week", multiple: true, required: false,
                 options: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-            input "modes", "mode", title: "Only when mode is", multiple: true, required: false
-            input "sendPushMessage", "enum", title: "Send a push notification?", metadata: [values:["Yes","No"]], required: false
-            input "phone", "phone", title: "Send a Text Message?", required: false
+        }
+
+        section([mobileOnly:true]) {
+            label title: "Assign a name", required: false
+            mode title: "Set for specific mode(s)", required: false
         }
     }
 }
 
 def installed()
 {
-    subscribe(master, "switch", switchHandler, [filterEvents: false])
+    initialize()
 }
 
 def updated()
 {
     unsubscribe()
+    initialize()
+}
+
+def initialize()
+{
+    if (customName) {
+        state.currentAppLabel = customName
+    }
     subscribe(master, "switch", switchHandler, [filterEvents: false])
 }
 
 def switchHandler(evt) {
     log.info evt.value
 
-    if (daysOk && modeOk) {
+    if (allOk) {
         // use Event rather than DeviceState because we may be changing DeviceState to only store changed values
         def recentStates = master.eventsSince(new Date(now() - 4000), [all:true, max: 10]).findAll{it.name == "switch"}
         log.debug "${recentStates?.size()} STATES FOUND, LAST AT ${recentStates ? recentStates[0].dateCreated : ''}"
@@ -76,13 +103,15 @@ def switchHandler(evt) {
             if (evt.value == "on" && lastTwoStatesWere("on", recentStates, evt)) {
                 log.debug "detected two taps, execute ON phrase"
                 location.helloHome.execute(settings.onPhrase)
-                def message = "${location.name} executed ${settings.onPhrase}"
+                def message = "${location.name} executed ${settings.onPhrase} because ${evt.title} was tapped twice."
                 send(message)
+                flashLights()
             } else if (evt.value == "off" && lastTwoStatesWere("off", recentStates, evt)) {
                 log.debug "detected two taps, execute OFF phrase"
                 location.helloHome.execute(settings.offPhrase)
-                def message = "${location.name} executed ${settings.offPhrase}"
+                def message = "${location.name} executed ${settings.offPhrase} because ${evt.title} was tapped twice."
                 send(message)
+                flashLights()
             }
         }
         else {
@@ -122,6 +151,50 @@ private send(msg) {
     log.debug msg
 }
 
+private flashLights() {
+    def doFlash = true
+    def onFor = onFor ?: 200
+    def offFor = offFor ?: 200
+    def numFlashes = numFlashes ?: 2
+
+    if (state.lastActivated) {
+        def elapsed = now() - state.lastActivated
+        def sequenceTime = (numFlashes + 1) * (onFor + offFor)
+        doFlash = elapsed > sequenceTime
+    }
+
+    if (doFlash) {
+        state.lastActivated = now()
+        def initialActionOn = flashLights.collect{it.currentSwitch != "on"}
+        def delay = 1L
+        numFlashes.times {
+            flashLights.eachWithIndex {s, i ->
+                if (initialActionOn[i]) {
+                    s.on(delay: delay)
+                }
+                else {
+                    s.off(delay:delay)
+                }
+            }
+            delay += onFor
+            flashLights.eachWithIndex {s, i ->
+                if (initialActionOn[i]) {
+                    s.off(delay: delay)
+                }
+                else {
+                    s.on(delay:delay)
+                }
+            }
+            delay += offFor
+        }
+    }
+}
+
+// execution filter methods
+private getAllOk() {
+    modeOk && daysOk && timeOk
+}
+
 private getModeOk() {
     def result = !modes || modes.contains(location.mode)
     result
@@ -143,7 +216,32 @@ private getDaysOk() {
     result
 }
 
-private hideOptionsSection() {
-    (days || modes) ? false : true
+private getTimeOk() {
+    def result = true
+    if (starting && ending) {
+        def currTime = now()
+        def start = timeToday(starting).time
+        def stop = timeToday(ending).time
+        result = start < stop ? currTime >= start && currTime <= stop : currTime <= stop || currTime >= start
+    }
+    log.trace "timeOk = $result"
+    result
 }
+
+private hideOptionsSection() {
+    (phone || starting || ending || flashLights || customName || days || modes) ? false : true
+}
+
+private hhmm(time, fmt = "h:mm a")
+{
+    def t = timeToday(time, location.timeZone)
+    def f = new java.text.SimpleDateFormat(fmt)
+    f.setTimeZone(location.timeZone ?: timeZone(time))
+    f.format(t)
+}
+
+private timeIntervalLabel() {
+    (starting && ending) ? hhmm(starting) + "-" + hhmm(ending, "h:mm a z") : ""
+}
+
 
